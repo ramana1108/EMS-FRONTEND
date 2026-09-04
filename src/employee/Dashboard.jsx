9968bf8c556ff10481b779fc943c1ab7dec23baf
Akshaya <abiakshaya206@gmail.com>
2026-09-05 00:00:14 +0530

    change in the error faced in render

--- src/employee/Dashboard.jsx
+++ src/employee/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api";
+// import api from "../api";
 import FooterNavigation from "../components/FooterNavigation";
 import Header from "../components/Header";
 import PredictiveSearchBar from "../components/PredictiveSearchBar";
